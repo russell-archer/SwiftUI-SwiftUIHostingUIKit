f3e4a4fc89155601b95be29771bd79a457bb06f2
russell-archer <russell.archer@mac.com>
2019-08-20 18:38:10 +0100

    Update for Xcode 11 Beta 6

---
+++ README.md
@@ -0,0 +1,8 @@
+# SwiftUI hosting UIKit Demo
+## x
+**(Updated for Xcode 11 Beta 6)**
+
+This demo xxxx.
+
+``` swift
+```
--- SwiftUI-SwiftUIHostingUIKit/ContentView.swift
+++ SwiftUI-SwiftUIHostingUIKit/ContentView.swift
@@ -5,12 +5,35 @@
 //  Created by Russell Archer on 19/08/2019.
 //  Copyright © 2019 Russell Archer. All rights reserved.
 //
+// Tested and working with Xcode 11 Beta 6 on Mojave 10.14.6
+//
 
 import SwiftUI
 
+/// Wrap a UITextView using UIViewRepresentable to create a TextView that's
+/// directly useable in SwiftUI
+struct TextView: UIViewRepresentable {
+    let text: String
+
+    func makeUIView(context: Context) -> UITextView {
+        let tv = UITextView()
+        tv.backgroundColor = UIColor.green
+        tv.isEditable = true
+
+        return tv
+    }
+
+    func updateUIView(_ textView: UITextView, context: Context) {
+        textView.text = self.text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            TextView(text: "Hello!")  // Use our TextView - simple!
+            Spacer()
+        }
     }
 }
 
